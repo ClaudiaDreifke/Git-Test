2b416296be2f8f774f495422f197c447291edc61
claudia <claudia.dreifke@gmail.com>
2022-06-29 08:50:39 +0200

    Add IntelliJ Test

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test funktioniert");
+        System.out.println("Git Test funktioniert auch mit Intellij?");
     }
 }
