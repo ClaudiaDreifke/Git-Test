4fb22dd83780b96606c58ec4202c9449c2864eec
claudia <claudia.dreifke@gmail.com>
2022-06-29 10:58:19 +0200

    Correct GitHub line

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test funktioniert");
         System.out.println("Git Test funktioniert auch mit Intellij?");
-        System.out.println("Github dazu")
+        System.out.println("Github dazu");
     }
 }
